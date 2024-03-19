10f78f29e78c8585f3a573d27dcfbca4aa971bfd
Jawadgem <jawad_nasir@msn.com>
2024-03-20 02:09:22 +0500

    Push My Calculator and Share yout view with me

---
+++ index.ts
@@ -0,0 +1,31 @@
+#! urs/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter first nummber", type: "number", name: "fisrtNumber" },
+  { message: "Enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operators to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Substraction", "Multiplication", "Division"],
+  },
+]);
+
+// condition statement
+
+if (answer.operator === "Addition") {
+    console.log(answer.fisrtNumber + answer.secondNumber);
+} else if (answer.operator === "Substraction") {
+    console.log(answer.fisrtNumber - answer.secondNumber);
+}
+
+else if (answer.operator === "Multiplication") {
+    console.log(answer.fisrtNumber * answer.secondNumber);
+}
+
+else if (answer.operator === "Division") {
+    console.log(answer.fisrtNumber / answer.secondNumber);
+}
+else { console.log("Please select valid operator")}
